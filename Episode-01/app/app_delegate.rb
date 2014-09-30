c31e02b8dbb8d315c0db75f6039564a3814edd8a
zhulinpinyu <mulixiang199016@gmail.com>
2014-09-30 16:30:07 +0800

    table basic done

--- Episode-01/app/app_delegate.rb
+++ Episode-01/app/app_delegate.rb
@@ -1,5 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @languages_controller = LanguagesController.alloc.initWithNibName(nil, bundle: nil)
+    @window.rootViewController = @languages_controller
+    @window.makeKeyAndVisible
     true
   end
 end
