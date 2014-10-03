134fb5fa9c7be3ce9d23e5f0dee4406a386030df
zhulinpinyu <mulixiang199016@gmail.com>
2014-10-03 18:58:27 +0800

    add loginv1_controller

--- Episode-03/app/app_delegate.rb
+++ Episode-03/app/app_delegate.rb
@@ -1,5 +1,23 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+    @window.rootViewController = klazz.alloc.init
     true
+  end
+
+  def klazz
+    part = ENV['PART']
+    if part.nil?
+      part = '6'
+      puts "Pls use default part #{part}"
+    else
+      puts "Using part #{part}"
+    end
+
+    case part
+    when '1'
+      Loginv1Controller
+    end
   end
 end
