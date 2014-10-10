24d7d91c896b54af8cfe5e43a0a0929ab02009eb
zhulinpinyu <mulixiang199016@gmail.com>
2014-10-10 12:23:21 +0800

    CAN NOT Send Email

--- Episode-09/app/controllers/login_help_controller.rb
+++ Episode-09/app/controllers/login_help_controller.rb
@@ -2,6 +2,7 @@ class LoginHelpController < UIViewController
 
   def viewDidLoad
     super
+    @presented_by = self.presentingViewController
     rmq.stylesheet = LoginHelpControllerStylesheet
     rmq(self.view).apply_style :root_view
 
@@ -16,7 +17,33 @@ class LoginHelpController < UIViewController
 
       q.append(UILabel, :more_help_label)
       q.append(UIButton, :more_help_note).on(:tap) do |_|
-        
+        self.dismissViewControllerAnimated(true, completion: -> { present_email_editor })
+      end
+    end
+  end
+
+  def present_email_editor
+    if MFMailComposeViewController.canSendMail
+      mail_controller = MFMailComposeViewController.alloc.init
+      if mail_controller
+        mail_controller.mailComposeDelegate = self;
+        mail_controller.setToRecipients(['support@lg.com'])
+        mail_controller.setSubject("Learn RubyMotion - Login help")
+        if @presented_by
+          @presented_by.presentViewController mail_controller, animated: true, completion: nil
+        else
+          App.alert "@_@ ERROR."
+        end
+      end
+    else
+      App.alert "@_@ ERROR."
+    end
+  end
+
+  def mailComposeController(controller, didFinishWithResult: result, error: error)
+    @presented_by.dismissViewControllerAnimated true, completion: -> do
+      if result == MFMailComposeResultSent
+        App.alert 'Thank you for your email, we will get back to you as soon as possible.'
       end
     end
   end
