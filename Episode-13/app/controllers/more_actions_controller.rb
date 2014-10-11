9c91793cb3b0559ec8c151447dd68e10999b020f
zhulinpinyu <mulixiang199016@gmail.com>
2014-10-11 15:27:21 +0800

    add gem bubble-wrap for App.alert

---
+++ Episode-13/app/controllers/more_actions_controller.rb
@@ -0,0 +1,20 @@
+class MoreActionsController < BaseLanguagesController
+  def init
+    super
+    self.tabBarItem = UITabBarItem.alloc.initWithTitle('More...', image: rmq.image.resource('lightbulb'), tag: 0)
+    self.title = 'More...'
+    self
+  end
+
+  def viewDidLoad
+    navigationItem.title = "More..."
+    view.dataSource = view.delegate = self
+    @data = []
+    @keys = @data.map { |r| r.name }
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER_FOR_MORE"
+    super
+  end
+end
