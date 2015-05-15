2a0d188d46b9678685287a1244f35d802bb6658e
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-16 00:36:30 +0800

    My First Layout (Use Motion-kit)

---
+++ ProMotionDemo/app/layouts/home_layout.rb
@@ -0,0 +1,11 @@
+class HomeLayout < MK::Layout
+
+  def layout
+    root :main do
+    end
+  end
+
+  def main_style
+    background_color "#F9F9F9".uicolor
+  end
+end
--- ProMotionDemo/app/screens/home_screen.rb
+++ ProMotionDemo/app/screens/home_screen.rb
@@ -2,11 +2,7 @@ class HomeScreen < PM::Screen
   title "Home"
 
   def on_load
-    set_nav_bar_button :right, title: "Help", action: :show_help
-  end
-
-  def show_help
-    open HelpScreen
+    @layout = HomeLayout.new(root: self.view).build
   end
 
 end
