5a08e265a1478b4fa7aa31ef5567e9e9acd632de
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-19 17:18:08 +0800

    MapBox Work Done

--- MapBoxDemo/app/app_delegate.rb
+++ MapBoxDemo/app/app_delegate.rb
@@ -3,6 +3,6 @@ class AppDelegate < PM::Delegate
   status_bar true, animation: :fade
 
   def on_load(app, options)
-    open HomeScreen.new(nav_bar: true)
+    open HomeScreen.new(nav_bar: false)
   end
 end
--- MapBoxDemo/app/screens/home_screen.rb
+++ MapBoxDemo/app/screens/home_screen.rb
@@ -1,8 +1,15 @@
 class HomeScreen < PM::Screen
+  MAPBOX_KEY = "pk.eyJ1Ijoiemh1bGlucGlueXUiLCJhIjoieWJDdWwzTSJ9.PfVmp2AcPEzrCMvejwfPDA"
   title "MapBox"
   stylesheet HomeScreenStylesheet
 
   def on_load
     @hello_world = append!(UILabel, :hello_world)
+    RMConfiguration.sharedInstance.setAccessToken(MAPBOX_KEY)
+    tile_source = RMMapboxSource.alloc.initWithMapID("mapbox.streets")
+    map_view = RMMapView.alloc.initWithFrame(self.view.bounds, andTilesource: tile_source)
+    map_view.zoom = 17
+    map_view.centerCoordinate = CLLocationCoordinate2DMake(22.53,114.02)
+    self.view = map_view
   end
 end
