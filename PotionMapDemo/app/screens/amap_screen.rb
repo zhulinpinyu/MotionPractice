c81cb6f64f0350fdad1212acad6c8b2b979c87a0
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-19 10:34:15 +0800

    annotation with action

--- PotionMapDemo/app/screens/amap_screen.rb
+++ PotionMapDemo/app/screens/amap_screen.rb
@@ -2,11 +2,30 @@ class AmapScreen < PM::MapScreen
   title "AMap"
   stylesheet AmapScreenStylesheet
 
+  start_position(latitude: 22.53, longitude: 114.03, radius: 1)
+
+  tap_to_add(length: 1.5, annotation: {
+    title: "MLX Dropped in",
+    animates_drop: true,
+    action: :show_details
+  })
+
   def on_load
   end
 
   def annotation_data
-    []
+    [{
+      coordinate: CLLocationCoordinate2DMake(22.53, 114.03),
+      title: "Rainbow Falls",
+      subtitle: "Nantahala National Forest",
+      image: UIImage.imageNamed("icon-60"),
+      action: :show_details
+    }]
+  end
+
+  def show_details
+    selected = selected_annotations.first
+    p selected.title
   end
 
 
