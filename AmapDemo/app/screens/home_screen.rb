719084d8f0f38d5487638a8729b5db08fad83c02
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-29 11:26:46 +0800

    add marker

--- AmapDemo/app/screens/home_screen.rb
+++ AmapDemo/app/screens/home_screen.rb
@@ -6,27 +6,15 @@ class HomeScreen < PM::Screen
   def on_load
     MAMapServices.sharedServices.apiKey = AMAP_KEY
     map_view = MAMapView.alloc.initWithFrame(CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds)))
-    coordinate = {latitude: 22.53, longitude: 114.03}
+    coordinate = {latitude: 22.535, longitude: 114.03}
     map_view.setCenterCoordinate(CLLocationCoordinate2DMake(coordinate[:latitude],coordinate[:longitude]))
     map_view.setZoomLevel(17, animated: false)
     #map_view.delegate = self
+    annotation = MAPointAnnotation.alloc.init
+    annotation.coordinate = CLLocationCoordinate2DMake(22.533773, 114.029946)
+    annotation.title = "KFC"
+    annotation.subtitle = "KFC TA"
+    map_view.addAnnotation(annotation)
     self.view = map_view
-  end
-
-  # You don't have to reapply styles to all UIViews, if you want to optimize, another way to do it
-  # is tag the views you need to restyle in your stylesheet, then only reapply the tagged views, like so:
-  #   def logo(st)
-  #     st.frame = {t: 10, w: 200, h: 96}
-  #     st.centered = :horizontal
-  #     st.image = image.resource('logo')
-  #     st.tag(:reapply_style)
-  #   end
-  #
-  # Then in will_animate_rotate
-  #   find(:reapply_style).reapply_styles#
-
-  # Remove the following if you're only using portrait
-  def will_animate_rotate(orientation, duration)
-    find.all.reapply_styles
   end
 end
