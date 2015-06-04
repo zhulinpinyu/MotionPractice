b348bd5f3f557daef11274cd7623847a7c203e35
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-04 09:43:21 +0800

    add nav bar and action with capture

--- WebAmapDemo/app/screens/home_screen.rb
+++ WebAmapDemo/app/screens/home_screen.rb
@@ -2,7 +2,15 @@ class HomeScreen < PM::WebScreen
   title "Web Amap"
   stylesheet HomeScreenStylesheet
 
+  def on_load
+    set_nav_bar_button :left, system_item: :camera, action: :capture
+  end
+
   def content
     NSURL.URLWithString("http://localhost/~lixiangmu/openlayer3/amap/")
+  end
+
+  def capture
+    UIImageWriteToSavedPhotosAlbum(image.from_view(self.view), nil,nil,nil)
   end
 end
