ad217a3f82e9b17b34ae80d667f521ac00f3eb6f
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-07 23:49:26 +0800

    Clear code

--- GCDDemo/app/screens/home_screen.rb
+++ GCDDemo/app/screens/home_screen.rb
@@ -1,36 +1,22 @@
 class HomeScreen < PM::Screen
-  title "Your title here"
+  title "多线程技术"
   stylesheet HomeScreenStylesheet
 
   def on_load
-    set_nav_bar_button :left, system_item: :camera, action: :nav_left_button
-    set_nav_bar_button :right, title: "Right", action: :nav_right_button
-
+    init_nav
     @hello_world = append!(UILabel, :hello_world)
   end
 
+  def init_nav
+    set_nav_bar_button :left, system_item: :camera, action: :nav_left_button
+    set_nav_bar_button :right, title: "→", action: :nav_right_button
+  end
+
   def nav_left_button
-    mp 'Left button'
+    mp "Left"
   end
 
   def nav_right_button
-    mp 'Right button'
-  end
-
-  # You don't have to reapply styles to all UIViews, if you want to optimize, another way to do it
-  # is tag the views you need to restyle in your stylesheet, then only reapply the tagged views, like so:
-  #   def logo(st)
-  #     st.frame = {t: 10, w: 200, h: 96}
-  #     st.centered = :horizontal
-  #     st.image = image.resource('logo')
-  #     st.tag(:reapply_style)
-  #   end
-  #
-  # Then in will_animate_rotate
-  #   find(:reapply_style).reapply_styles#
-
-  # Remove the following if you're only using portrait
-  def will_animate_rotate(orientation, duration)
-    find.all.reapply_styles
+    mp "Right"
   end
 end
