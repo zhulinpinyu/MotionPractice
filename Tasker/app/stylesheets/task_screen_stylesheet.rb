2fa7e6d5dd8188fa8e942501f6b5ae5e901c2722
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-09 23:43:44 +0800

    add Fill Task UI

--- Tasker/app/stylesheets/task_screen_stylesheet.rb
+++ Tasker/app/stylesheets/task_screen_stylesheet.rb
@@ -13,11 +13,33 @@ class TaskScreenStylesheet < ApplicationStylesheet
 
   def task_view(st)
     st.frame = :full
-    st.background_color = color.gray
+    st.background_color = color.white
   end
 
   def text_field(st)
-    st.frame = [[20,100],[100,30]]
-    st.placeholder = "WER"
+    st.frame = [[20,20],[280,40]]
+    st.placeholder = "e.g. Do You Want"
+    st.layer.borderWidth = 1
+    st.layer.borderColor = color.gray.CGColor
+    st.layer.cornerRadius = 3
+  end
+
+  def note_label(st)
+    st.frame = {l:20, t: 80, w: 280, h:40}
+    st.text = "Notes"
+    st.font = UIFont.boldSystemFontOfSize(14)
+    st.text_color = color.dark_gray
+  end
+
+  def text_view(st)
+    st.frame = {l:20, t:125, w: 280, h:100}
+    st.layer.borderWidth = 1
+    st.layer.borderColor = color.gray.CGColor
+    st.layer.cornerRadius = 3
+    st.font = UIFont.systemFontOfSize(14)
+  end
+
+  def date_picker(st)
+    st.frame = {l:0, t:230, w: 320, h:100}
   end
 end
--- Tasker/app/views/task_view.rb
+++ Tasker/app/views/task_view.rb
@@ -1,5 +1,8 @@
 class TaskView < UIScrollView
   def rmq_build
-    append!(UITextField, :text_field)
+    append!(TextField, :text_field)
+    append!(UILabel, :note_label)
+    append!(UITextView, :text_view)
+    append!(UIDatePicker, :date_picker)
   end
 end
